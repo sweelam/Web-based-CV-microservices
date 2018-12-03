168b90cb87a2e40ddbb0d8362fc683ee0fe821e0
Sweelam <mohamed_bob2007@yahoo.com>
2018-12-03 22:17:32 +0400

    Add logging aspect

--- cv-portal/src/main/java/com/me/website/model/repository/MyInfoRepo.java
+++
@@ -1,8 +0,0 @@
-package com.me.website.model.repository;
-
-import com.me.website.model.entity.MyInfoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MyInfoRepo extends JpaRepository<MyInfoEntity, Integer>{
-
-}
