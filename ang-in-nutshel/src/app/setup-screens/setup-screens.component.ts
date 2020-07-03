6880f6695ea9827d245abb9a24141d77f2a7a0e9
Radwa Mamdouh <rmamdouh7979@gmail.com>
2020-07-03 12:08:01 +0200

    refactor&restyle[pro files]: refctoring and styling project components

--- ang-in-nutshel/src/app/setup-screens/setup-screens.component.ts
+++ ang-in-nutshel/src/app/setup-screens/setup-screens.component.ts
@@ -1,45 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, FormControl } from "@angular/forms";
 
-import { SetupScreensService } from './setup-screens.service'
-import { UserJob } from './user';
-import { ErrorHandler } from '../error-handler/error-handler';
-import { SwalShowUtil } from '../error-handler/error-show-util';
-import { Router } from '@angular/router';
+import { SetupScreensService } from "./setup-screens.service";
+import { UserJob } from "./user";
+import { ErrorHandler } from "../error-handler/error-handler";
+import { SwalShowUtil } from "../error-handler/error-show-util";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-setup-screens',
-  templateUrl: './setup-screens.component.html',
-  styleUrls: ['../login/login.component.css']
+  selector: "app-setup-screens",
+  templateUrl: "./setup-screens.component.html",
+  styleUrls: ["../login/login.component.css", "./setup-screens.component.css"],
 })
 export class SetupScreensComponent implements OnInit {
-
   formNo: number[] = [];
   count: number;
   userForm: FormGroup;
   userId: number;
 
-  constructor(private setupService: SetupScreensService,
-    private formBuilder: FormBuilder, private route: Router) {
-  }
+  constructor(
+    private setupService: SetupScreensService,
+    private formBuilder: FormBuilder,
+    private route: Router
+  ) {}
 
   ngOnInit() {
     if (sessionStorage.getItem("userId")) {
-      this.userId = +sessionStorage.getItem("userId")
-      this.setupService.getInfoById(this.userId)
-        .subscribe((data: UserJob) => {
-          this.userForm.get('fullName').setValue(data.fullName)
-          this.userForm.get('email').setValue(data.email)
-          this.userForm.get('mobile').setValue(data.mobile)
-          this.userForm.get('address').setValue(data.address)
-          this.userForm.get('title').setValue(data.title)
-          this.userForm.get('from').setValue(data.from)
-          this.userForm.get('to').setValue(data.to)
-          this.userForm.get('jobDesc').setValue(data.jobDesc)
-        })
+      this.userId = +sessionStorage.getItem("userId");
+      this.setupService.getInfoById(this.userId).subscribe((data: UserJob) => {
+        this.userForm.get("fullName").setValue(data.fullName);
+        this.userForm.get("email").setValue(data.email);
+        this.userForm.get("mobile").setValue(data.mobile);
+        this.userForm.get("address").setValue(data.address);
+        this.userForm.get("title").setValue(data.title);
+        this.userForm.get("from").setValue(data.from);
+        this.userForm.get("to").setValue(data.to);
+        this.userForm.get("jobDesc").setValue(data.jobDesc);
+      });
     }
 
-    this.buildSetupForm()
+    this.buildSetupForm();
   }
 
   buildSetupForm() {
@@ -51,7 +51,7 @@ export class SetupScreensComponent implements OnInit {
       title: new FormControl(null),
       from: new FormControl(null),
       to: new FormControl(null),
-      jobDesc: new FormControl(null)
+      jobDesc: new FormControl(null),
     });
   }
 
@@ -63,22 +63,39 @@ export class SetupScreensComponent implements OnInit {
   onSubmit() {
     // Convert object into text
     var userInfoText = this.userForm.value as UserJob;
-    this.setupService.saveJobDesc(userInfoText)
-      .subscribe((data: any) => {
+    this.setupService.saveJobDesc(userInfoText).subscribe(
+      (data: any) => {
         if (data.status == 201) {
-          SwalShowUtil.popupError('Success', 'Your information is submitted correctly', 'OK', 'success');
-          this.route.navigate(['/home', this.userId])
+          SwalShowUtil.popupError(
+            "Success",
+            "Your information is submitted correctly",
+            "OK",
+            "success"
+          );
+          this.route.navigate(["/home", this.userId]);
         }
-      }, (error: ErrorHandler) => {
+      },
+      (error: ErrorHandler) => {
         if (error.status == 304)
-          SwalShowUtil.popupError('Warning', 'Your information is not submitted correctly', 'OK', 'warning');
+          SwalShowUtil.popupError(
+            "Warning",
+            "Your information is not submitted correctly",
+            "OK",
+            "warning"
+          );
         else
-          SwalShowUtil.popupError('Error', 'error.error.message', 'OK', 'error');
-      });
+          SwalShowUtil.popupError(
+            "Error",
+            "error.error.message",
+            "OK",
+            "error"
+          );
+      }
+    );
   }
 
   addNewForm() {
-    this.formNo.push(this.count++)
+    this.formNo.push(this.count++);
   }
 
   reset() {
