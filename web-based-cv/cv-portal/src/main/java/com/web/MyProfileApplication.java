729a11ba03de9cacc0a9c3ead0bb305619bbd7a1
sweelam <mohamedsweelam@fci.helwan.edu.eg>
2021-05-12 18:07:27 +0400

    Fix admin server issue

--- web-based-cv/cv-portal/src/main/java/com/web/MyProfileApplication.java
+++ web-based-cv/cv-portal/src/main/java/com/web/MyProfileApplication.java
@@ -7,12 +7,10 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 
 @SpringBootApplication(scanBasePackages= {"com.web"})
 @EnableEurekaClient
-@EnableResourceServer
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @RefreshScope
 public class MyProfileApplication extends SpringBootServletInitializer {
